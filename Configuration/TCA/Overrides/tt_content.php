4849869381fb39c58ffe8624bee10063b396edc9
chris@kubuntu <pschorr.christian@gmail.com>
2018-11-24 13:52:18 +0100

    #1: Auswahlmöglichkeiten von Space Before / After angepasst

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,5 @@
+<?php
+
+// Zuruecksetzen der Werte Space Before / After, damit diese komplett ueber PageTs befuellt werden koennen
+$GLOBALS['TCA']['tt_content']['columns']['space_before_class']['config']['items'] = [];
+$GLOBALS['TCA']['tt_content']['columns']['space_after_class']['config']['items'] = [];
