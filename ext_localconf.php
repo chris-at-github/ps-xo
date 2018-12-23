f409a3861f5c2ac0d1c974cf0339a2032af12985
chris@kubuntu <pschorr.christian@gmail.com>
2018-12-23 12:19:18 +0100

    #5: Basis Konfiguration für den Editor hinterlegt

--- ext_localconf.php
+++ ext_localconf.php
@@ -9,3 +9,6 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPo
 
 // Hook is called after Caching / pages with COA_/USER_INT objects.
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] = \Ps\Xo\Service\PostProcessService::class . '->outputNoCache';
+
+// Konfiguration fuer den (CKE) Editor im Backend
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['xo'] = 'EXT:xo/Configuration/RTE/Xo.yaml';
