e8dd01bc2e2ddcea9b678566a97c8ee64ae4e46c
chris@kubuntu <pschorr.christian@gmail.com>
2018-12-28 21:15:51 +0100

    #6: Datenbankfeld in TT-Content erstellt

--- ext_tables.sql
+++ ext_tables.sql
@@ -3,5 +3,6 @@
 #
 CREATE TABLE tt_content (
   tx_xo_variant int(11) unsigned DEFAULT '0',
-  tx_xo_header_class int(11) unsigned DEFAULT '0'
+  tx_xo_header_class int(11) unsigned DEFAULT '0',
+  tx_xo_file int (11) unsigned DEFAULT '0' NOT NULL
 );
