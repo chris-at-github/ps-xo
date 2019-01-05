f6edc99b80e94f9d3c6b4b367cc050e751feef85
chris@kubuntu <pschorr.christian@gmail.com>
2019-01-05 11:49:59 +0100

    #9: Hinzufuegen der Bild-Paletteninformationen im TCA; Konfiguration für Crop-Varianten Fullscreen und Thumbnail hinzugefuegt

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -120,44 +120,69 @@ $GLOBALS['TCA']['tt_content']['types']['xo_gallery'] = [
 					'appearance' => [
 						'createNewRelationLinkTitle' => 'LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:media.addFileReference'
 					],
-					'foreign_types' => [
-						'0' => [
-							'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
-						],
-						\TYPO3\CMS\Core\Resource\File::FILETYPE_TEXT => [
-							'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
-						],
-						\TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
-							'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
-						],
-						\TYPO3\CMS\Core\Resource\File::FILETYPE_AUDIO => [
-							'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
-						],
-						\TYPO3\CMS\Core\Resource\File::FILETYPE_VIDEO => [
-							'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
-						],
-						\TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => [
-							'showitem' => '
-                        --palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
-                        --palette--;;filePalette'
-						]
-					],
 					'maxitems' => 999
 				],
 				$GLOBALS['TYPO3_CONF_VARS']['GFX']['imagefile_ext']
 			),
 		]
 	]
+];
+
+$GLOBALS['TCA']['tt_content']['types']['xo_gallery']['columnsOverrides']['tx_xo_file']['config']['overrideChildTca']['types'] = [
+	'0' => [
+		'showitem' => '
+							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+							--palette--;;filePalette'
+	],
+	\TYPO3\CMS\Core\Resource\File::FILETYPE_TEXT => [
+		'showitem' => '
+							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+							--palette--;;filePalette'
+	],
+	\TYPO3\CMS\Core\Resource\File::FILETYPE_IMAGE => [
+		'showitem' => '
+							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+							--palette--;;filePalette'
+	],
+	\TYPO3\CMS\Core\Resource\File::FILETYPE_AUDIO => [
+		'showitem' => '
+							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+							--palette--;;filePalette'
+	],
+	\TYPO3\CMS\Core\Resource\File::FILETYPE_VIDEO => [
+		'showitem' => '
+							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+							--palette--;;filePalette'
+	],
+	\TYPO3\CMS\Core\Resource\File::FILETYPE_APPLICATION => [
+		'showitem' => '
+							--palette--;LLL:EXT:lang/locallang_tca.xlf:sys_file_reference.imageoverlayPalette;imageoverlayPalette,
+							--palette--;;filePalette'
+	]
+];
+
+// @see: https://www.clickstorm.de/blog/crop-funktion-fuer-bilder-in-typo3-8-7/
+$GLOBALS['TCA']['tt_content']['types']['xo_gallery']['columnsOverrides']['tx_xo_file']['config']['overrideChildTca']['columns']['crop']['config']['cropVariants'] = [
+	'fullsize' => [
+		'title' => 'LLL:EXT:xo/Resources/Private/Language/locallang_tca.xlf:ratio.fullsize',
+		'allowedAspectRatios' => [
+			'NaN' => [
+				'title' => 'LLL:EXT:core/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.free',
+				'value' => 0.0
+			],
+		],
+		'selectedRatio' => 'NaN',
+	],
+	'thumbnail' => [
+		'title' => 'LLL:EXT:xo/Resources/Private/Language/locallang_tca.xlf:ratio.thumbnail',
+		'allowedAspectRatios' => [
+			'NaN' => [
+				'title' => 'LLL:EXT:core/Resources/Private/Language/locallang_wizards.xlf:imwizard.ratio.free',
+				'value' => 0.0
+			],
+		],
+		'selectedRatio' => 'NaN',
+	],
 ];
 
 // ---------------------------------------------------------------------------------------------------------------------
