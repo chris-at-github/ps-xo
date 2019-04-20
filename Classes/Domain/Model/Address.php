4d88fbde2f683413c1d697f8eec19ec40e07f1f6
chris@kubuntu <pschorr.christian@gmail.com>
2019-04-20 23:49:20 +0200

    #11: Übernahme der zwei neuen Felder (Schema.org) ins Model

--- Classes/Domain/Model/Address.php
+++ Classes/Domain/Model/Address.php
@@ -8,4 +8,42 @@ namespace Ps\Xo\Domain\Model;
  * @entity
  */
 class Address extends \FriendsOfTYPO3\TtAddress\Domain\Model\Address {
+
+	/**
+	 * @var int
+	 */
+	protected $schemaOrgType;
+
+	/**
+	 * @var \TYPO3\CMS\Extbase\Domain\Model\FileReference
+	 */
+	protected $schemaOrgMedia;
+
+	/**
+	 * @return int
+	 */
+	public function getSchemaOrgType() {
+		return $this->schemaOrgType;
+	}
+
+	/**
+	 * @param int $schemaOrgType
+	 */
+	public function setSchemaOrgType($schemaOrgType) {
+		$this->schemaOrgType = $schemaOrgType;
+	}
+
+	/**
+	 * @return \TYPO3\CMS\Extbase\Domain\Model\FileReference
+	 */
+	public function getSchemaOrgMedia() {
+		return $this->schemaOrgMedia;
+	}
+
+	/**
+	 * @param \TYPO3\CMS\Extbase\Domain\Model\FileReference $schemaOrgMedia
+	 */
+	public function setSchemaOrgMedia(\TYPO3\CMS\Extbase\Domain\Model\FileReference $schemaOrgMedia) {
+		$this->schemaOrgMedia = $schemaOrgMedia;
+	}
 }
