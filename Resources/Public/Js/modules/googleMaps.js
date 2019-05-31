af8a71197879528fde133ff32595fa9f5f00a5ab
Chris <pschorr.christian@gmail.com>
2019-05-31 09:39:58 +0200

    #18: Intiale Map Einbindung

--- Resources/Public/Js/modules/googleMaps.js
+++ Resources/Public/Js/modules/googleMaps.js
@@ -11,13 +11,50 @@
 	'use strict';
 
 	var GoogleMaps = function(selector, options) {
-		this.initialize();
+		var _ = this;
 
-		console.log(selector);
+		_.selector = selector;
+
+		_.defaults = {
+			zoom: 10,
+			center: {
+				latitude: 0.0,
+				longitude: 0.0
+			},
+			controls: {
+				zoom: true,
+				mapType: false,
+				scale: true,
+				streetView: false,
+				rotate: false,
+				fullscreen: false
+			}
+		};
+
+		_.options = Object.assign(_.defaults, options);
+
+		_.initialize();
 	};
 
 	GoogleMaps.prototype.initialize = function() {
-		console.log('GoogleMaps::initialize');
+		var _ = this;
+		var nodes = document.querySelectorAll(this.selector);
+
+		nodes.forEach(function(node) {
+			var map = new google.maps.Map(node, {
+				center: {
+					lat: _.options.center.latitude,
+					lng: _.options.center.longitude
+				},
+				zoom: _.options.zoom,
+				zoomControl: _.options.controls.zoom,
+				mapTypeControl: _.options.controls.mapType,
+				scaleControl: _.options.controls.scale,
+				streetViewControl: _.options.controls.streetView,
+				rotateControl: _.options.controls.rotate,
+				fullscreenControl: _.options.controls.fullscreen
+			});
+		});
 	}
 
 	return GoogleMaps;
