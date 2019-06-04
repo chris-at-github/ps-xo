3bd06d8d67035c2afe2030ae1ff9b348356a687f
Chris <pschorr.christian@gmail.com>
2019-06-04 23:44:55 +0200

    #19: Anpassung Layout an IE11

--- Resources/Public/Js/modules/googleMaps.js
+++ Resources/Public/Js/modules/googleMaps.js
@@ -10,6 +10,16 @@
 }(function() {
 	'use strict';
 
+	// @see: https://www.mediaevent.de/javascript/for-in-foreach.html
+	if(window.NodeList && ! NodeList.prototype.forEach) {
+		NodeList.prototype.forEach = function(callback, thisArg) {
+			thisArg = thisArg || window;
+			for(var i = 0; i < this.length; i ++) {
+				callback.call(thisArg, this[i], i, this);
+			}
+		};
+	}
+
 	var GoogleMaps = function(selector, options) {
 		var _ = this;
 
