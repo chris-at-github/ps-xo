0b86568e4be77acd259247e2d667645ff6575528
Christian Pschorr <pschorr.christian@gmail.com>
2019-11-11 23:29:30 +0100

    Dynnamisches Page Layout anhand der Backend Auswahl (page -> layout)

---
+++ Classes/DataProcessing/PageLayoutProcessor.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Ps\Xo\DataProcessing;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
+use TYPO3\CMS\Frontend\ContentObject\DataProcessorInterface;
+
+/**
+ * Sorgt fuer die richtige Auswahl des Seitenlayout, je Backendeinstellung (page -> layout)
+ */
+class PageLayoutProcessor implements DataProcessorInterface {
+
+	/**
+	 * Object Manager
+	 *
+	 * @var \TYPO3\CMS\Extbase\Object\ObjectManagerInterface
+	 */
+	protected $objectManager;
+
+	/**
+	 * @return void
+	 */
+	public function __construct() {
+		$this->objectManager = GeneralUtility::makeInstance(\TYPO3\CMS\Extbase\Object\ObjectManager::class);
+	}
+
+	/**
+	 * Parst die Inhalte aller verknupeften Inhaltselemente
+	 *
+	 * @param ContentObjectRenderer $cObject The data of the content element or page
+	 * @param array $contentObjectConfiguration The configuration of Content Object
+	 * @param array $processorConfiguration The configuration of this processor
+	 * @param array $processedData Key/value store of processed data (e.g. to be passed to a Fluid View)
+	 * @return array the processed data as key/value store
+	 */
+	public function process(ContentObjectRenderer $cObject, array $contentObjectConfiguration, array $processorConfiguration, array $processedData)	{
+		if(isset($processorConfiguration['mapping.'][$processedData['data']['layout']]) === true) {
+			$processedData[$processorConfiguration['as']] = $processorConfiguration['mapping.'][$processedData['data']['layout']];
+		}
+ 
+		return $processedData;
+	}
+}
