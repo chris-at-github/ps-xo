d885437bc7512595a314a8d6e422a327a83bd422
Chris <pschorr.christian@gmail.com>
2019-11-21 23:12:12 +0100

    Recursive Suche nach dem korrekten Layout

--- Classes/DataProcessing/PageLayoutProcessor.php
+++ Classes/DataProcessing/PageLayoutProcessor.php
@@ -27,6 +27,32 @@ class PageLayoutProcessor implements DataProcessorInterface {
 	}
 
 	/**
+	 * @param array $processorConfiguration The configuration of this processor
+	 * @param array $processedData Key/value store of processed data (e.g. to be passed to a Fluid View)
+	 * @return int
+	 */
+	protected function getLayout($processorConfiguration, $processedData) {
+		$layout = $processedData['data']['layout'];
+
+		// wenn Layout nicht direkt zugeordnet ist
+		// wenn man sich nicht auf der obersten Ebene befindet
+		// wenn die recursive Suche aktiv ist
+		if(empty($layout) === true && $processedData['data']['pid'] !== 0 && (isset($processorConfiguration['recursive']) === true && (int) $processorConfiguration['recursive'] === 1)) {
+			$rootline = $this->objectManager->get(\TYPO3\CMS\Core\Utility\RootlineUtility::class, $processedData['data']['pid'])->get();
+
+			if(empty($rootline) === false) {
+				foreach($rootline as $page) {
+					if(empty($page['layout']) === false) {
+						return (int) $page['layout'];
+					}
+				}
+			}
+		}
+
+		return $layout;
+	}
+
+	/**
 	 * Parst die Inhalte aller verknupeften Inhaltselemente
 	 *
 	 * @param ContentObjectRenderer $cObject The data of the content element or page
@@ -36,8 +62,10 @@ class PageLayoutProcessor implements DataProcessorInterface {
 	 * @return array the processed data as key/value store
 	 */
 	public function process(ContentObjectRenderer $cObject, array $contentObjectConfiguration, array $processorConfiguration, array $processedData)	{
-		if(isset($processorConfiguration['mapping.'][$processedData['data']['layout']]) === true) {
-			$processedData[$processorConfiguration['as']] = $processorConfiguration['mapping.'][$processedData['data']['layout']];
+		$layout = $this->getLayout($processorConfiguration, $processedData);
+
+		if(isset($processorConfiguration['mapping.'][$layout]) === true) {
+			$processedData[$processorConfiguration['as']] = $processorConfiguration['mapping.'][$layout];
 	
 		} else {
 			$processedData[$processorConfiguration['as']] = $processorConfiguration['default'];
