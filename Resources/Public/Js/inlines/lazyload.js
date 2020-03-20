9cd75b44f4323126dc5909eb7d39a0cb98bbd604
Christian Pschorr <c.pschorr@datamints.com>
2020-03-20 22:51:58 +0100

    Implementierung Lazysizes Lazyload

--- Resources/Public/Js/inlines/lazyload.js
+++ Resources/Public/Js/inlines/lazyload.js
@@ -1,1 +1,34 @@
-console.log(yall);
+document.addEventListener('DOMContentLoaded', function() {
+	yall({
+		observeChanges: true,
+		lazyBackgroundClass: 'lazy-background',
+		lazyBackgroundLoaded: 'lazy-background-loaded',
+		events: {
+			// The object key is sent as the first argument to `addEventListener`,
+			// which is the event. The corresponding value can be the callback if you
+			// don't want to send any options to `addEventListener`.
+			load: function(event) {
+				if(event.target.classList.contains('lazy') === true && event.target.nodeName == 'IMG') {
+					event.target.classList.add('lazy-loaded');
+				}
+
+				console.log(event.target.nodeName);
+			}
+		}
+	});
+});
+
+
+window.lazySizesConfig = window.lazySizesConfig || {};
+
+window.lazySizesConfig.lazyClass = 'lazy';
+window.lazySizesConfig.loadingClass = 'lazy--loading';
+window.lazySizesConfig.loadedClass = 'lazy--loaded';
+
+document.addEventListener('lazybeforeunveil', function(event) {
+	var background = event.target.getAttribute('data-background-src');
+
+	if(background !== null) {
+		event.target.style.backgroundImage = 'url(' + background + ')';
+	}
+});
