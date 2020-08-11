5f8ffe1835890aecc821c42e22e2b464b131c469
Christian Pschorr <c.pschorr@datamints.com>
2020-08-11 13:02:48 +0200

    Neuen Pagetype Typolink zur Auswahl im Backend hinzugefügt

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -24,3 +24,40 @@ $tmpXoPagesColumns = [
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns('pages', $tmpXoPagesColumns);
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addFieldsToPalette('pages', 'media', '--linebreak--, tx_xo_flash', 'after:media');
+
+// ---------------------------------------------------------------------------------------------------------------------
+// Neuen Pagetyp Typolink
+//(function() {
+	$pageTypeTypolink = 120;
+
+	// Add new page type as possible select item:
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem(
+		'pages',
+		'doktype',
+		[
+			'LLL:EXT:xo/Resources/Private/Language/locallang_tca.xlf:tx_xo_pages.pagetype.typolink',
+			$pageTypeTypolink,
+			'EXT:xo/Resources/Public/Icons/xo-page-typolink.svg'
+		],
+		'1',
+		'after'
+	);
+
+	\TYPO3\CMS\Core\Utility\ArrayUtility::mergeRecursiveWithOverrule(
+		$GLOBALS['TCA']['pages'],
+		[
+			// add icon for new page type:
+			'ctrl' => [
+				'typeicon_classes' => [
+					$pageTypeTypolink => 'xo-page-typolink',
+				],
+			],
+			// add all page standard fields and tabs to your new page type
+			'types' => [
+				(string) $pageTypeTypolink => [
+					'showitem' => $GLOBALS['TCA']['pages']['types'][\TYPO3\CMS\Frontend\Page\PageRepository::DOKTYPE_LINK]['showitem']
+				]
+			]
+		]
+	);
+//});
--- ext_tables.php
+++ ext_tables.php
@@ -14,4 +14,28 @@ call_user_func(function($_EXTKEY) {
 	);
 
 	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_xo_domain_model_elements');
+
+	// Neuen Seitentyp Typolink erstellen
+	$pageTypeTypolink = 120;
+
+	// Add new page type:
+	$GLOBALS['PAGES_TYPES'][$pageTypeTypolink] = [
+		'type' => 'web',
+		'allowedTables' => '*',
+	];
+
+	// Provide icon for page tree, list view, ... :
+	\TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Imaging\IconRegistry::class)
+		->registerIcon(
+			'xo-page-typolink',
+			TYPO3\CMS\Core\Imaging\IconProvider\SvgIconProvider::class,
+			[
+				'source' => 'EXT:' . $_EXTKEY . '/Resources/Public/Icons/xo-page-typolink.svg',
+			]
+		);
+
+	// Allow backend users to drag and drop the new page type:
+	\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addUserTSConfig(
+		'options.pageTree.doktypesToShowInNewPageDragArea := addToList(' . $pageTypeTypolink . ')'
+	);
 }, $_EXTKEY);
