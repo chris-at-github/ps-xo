73d5a5a53894fa22f38c2b23ab3f8deeb58cfe80
Christian Pschorr <c.pschorr@datamints.com>
2020-11-11 23:42:16 +0100

    Haupt CSS Klasse selbstständig erkennen und in eventuellen Unterelementen ersetzen

--- Classes/Service/HtmlParserService.php
+++ Classes/Service/HtmlParserService.php
@@ -11,18 +11,20 @@ use TYPO3\CMS\Extbase\Utility\DebuggerUtility;
 class HtmlParserService {
 
 	/**
-	 * Ersetzt innerhalb eines UL|OL HTML Elements eine verschachtelte Klasse (options: class)
+	 * Ersetzt innerhalb eines UL|OL HTML Elements eine doppelt gesetzte Klasse auf (eventuellen) Unterelementen
 	 *
 	 * @param string $value
 	 * @param array $options
 	 */
 	public function fixListClassAttribute($value, $options = []) {
-		if(preg_match("/<" . $options['tag'] . "[^>]*>\K.*(?=<\/" . $options['tag'] . ">)/Uis", $value, $match) !== 0) {
-			$value = str_replace(
-				$match[0],
-				str_replace('<' . $options['tag'] . ' class="' . $options['class'] . '">', '<' . $options['tag'] . '>', $match[0]),
-				$value
-			);
+		if(preg_match("/(<" . $options['tag'] . "[^>]*>)\K.*(?=<\/" . $options['tag'] . ">)/Uis", $value, $match) !== 0) {
+			if(preg_match("/\s?class=\"(.*)\"\s?/Uis", $match[1], $classValue) !== 0) {
+				$value = str_replace(
+					$match[0],
+					str_replace('<' . $options['tag'] . ' class="' . $classValue[1] . '">', '<' . $options['tag'] . '>', $match[0]),
+					$value
+				);
+			}
 		}
 
 		return $value;
