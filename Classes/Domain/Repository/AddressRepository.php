f7de0a18997c151f489f28726a6d88a601ee3505
Christian Pschorr <c.pschorr@datamints.com>
2020-12-15 22:45:48 +0100

    Auslagerung von Duplicate Code in ein allgemeines Repository

--- Classes/Domain/Repository/AddressRepository.php
+++ Classes/Domain/Repository/AddressRepository.php
@@ -2,14 +2,12 @@
 
 namespace Ps\Xo\Domain\Repository;
 
-use TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException;
 use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
-use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 
 /**
  * The repository for the domain model Address
  */
-class AddressRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+class AddressRepository extends Repository {
 
 	/**
 	 * override the storagePid settings (do not use storagePid) of extbase
@@ -25,50 +23,12 @@ class AddressRepository extends \TYPO3\CMS\Extbase\Persistence\Repository {
 	 * @return array
 	 */
 	protected function getMatches($query, $options) {
-		$matches = [];
+		$matches = parent::getMatches($query, $options);
 
 		if(isset($options['content']) === true) {
 			$matches[] = $query->equals('txXoContent', (int) $options['content']);
 		}
 
-		if(isset($options['records']) === true) {
-
-			// bei Eingabe von festen IDs duerfen nur die IDs der Hauptsprache verwendet werden, Extbase kuemmert sich per
-			// Overlay um die korrekte Uebersetzung
-			$query->getQuerySettings()->setRespectSysLanguage(false);
-
-			// immer als Array auswerten
-			if(is_array($options['records']) === false) {
-				$options['records'] = [$options['records']];
-			}
-
-			$matches[] = $query->in('uid', $options['records']);
-		}
-
 		return $matches;
-	}
-
-	/**
-	 * @param array $options
-	 * @return QueryResultInterface
-	 * @throws InvalidQueryException
-	 */
-	public function findAll($options = []) {
-		$query = $this->createQuery();
-
-		if(empty($matches = $this->getMatches($query, $options)) === false) {
-			$query->matching($query->logicalAnd($matches));
-		}
-
-		return $query->execute();
-	}
-
-	/**
-	 * @param array $options
-	 * @return object
-	 * @throws InvalidQueryException
-	 */
-	public function find($options = []) {
-		return $this->findAll($options)->getFirst();
 	}
 }
---
+++ Classes/Domain/Repository/Repository.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Ps\Xo\Domain\Repository;
+
+use TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException;
+use TYPO3\CMS\Extbase\Persistence\Generic\Typo3QuerySettings;
+use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
+
+/**
+ * The repository for the domain model Pages
+ */
+class Repository extends \TYPO3\CMS\Extbase\Persistence\Repository {
+
+	/**
+	 * @param \TYPO3\CMS\Extbase\Persistence\QueryInterface $query
+	 * @param array $options
+	 * @return array
+	 */
+	protected function getMatches($query, $options) {
+		$matches = [];
+
+		if(isset($options['records']) === true) {
+
+			// bei Eingabe von festen IDs duerfen nur die IDs der Hauptsprache verwendet werden, Extbase kuemmert sich per
+			// Overlay um die korrekte Uebersetzung
+			$query->getQuerySettings()->setRespectSysLanguage(false);
+
+			// immer als Array auswerten
+			if(is_array($options['records']) === false) {
+				$options['records'] = [$options['records']];
+			}
+
+			$matches[] = $query->in('uid', $options['records']);
+		}
+
+		return $matches;
+	}
+
+	/**
+	 * @param array $options
+	 * @return QueryResultInterface
+	 * @throws InvalidQueryException
+	 */
+	public function findAll($options = []) {
+		$query = $this->createQuery();
+
+		if(empty($matches = $this->getMatches($query, $options)) === false) {
+			$query->matching($query->logicalAnd($matches));
+		}
+
+		return $query->execute();
+	}
+
+	/**
+	 * @param array $options
+	 * @return object
+	 * @throws InvalidQueryException
+	 */
+	public function find($options = []) {
+		return $this->findAll($options)->getFirst();
+	}
+}
