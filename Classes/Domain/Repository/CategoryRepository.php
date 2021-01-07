79852b8e4e54210875443786744d0f7c4f0f4787
Christian Pschorr <c.pschorr@datamints.com>
2021-01-07 23:40:35 +0100

    Erstellung CategoryRepository mit Abfrage auf die Elternkategorie

---
+++ Classes/Domain/Repository/CategoryRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Ps\Xo\Domain\Repository;
+
+/**
+ * The repository for the domain model sys_category
+ */
+class CategoryRepository extends Repository {
+
+	/**
+	 * @param \TYPO3\CMS\Extbase\Persistence\QueryInterface $query
+	 * @param array $options
+	 * @return array
+	 */
+	protected function getMatches($query, $options) {
+		$matches = parent::getMatches($query, $options);
+
+		if(isset($options['parent']) === true) {
+			$matches[] = $query->equals('parent', (int) $options['parent']);
+		}
+
+		return $matches;
+	}
+}
