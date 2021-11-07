ce6deb6ba94098f812d485b7f129443954a08863
Christian Pschorr <c.pschorr@datamints.com>
2021-11-07 21:54:19 +0100

    Übersetzungsverhalten des Hidden- und Crop-Fields in sys_file_reference Datensätzen angepasst

---
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -0,0 +1,4 @@
+<?php
+
+$GLOBALS['TCA']['sys_file_reference']['columns']['crop']['config']['behaviour']['allowLanguageSynchronization'] = true;
+$GLOBALS['TCA']['sys_file_reference']['columns']['hidden']['config']['behaviour']['allowLanguageSynchronization'] = true;
